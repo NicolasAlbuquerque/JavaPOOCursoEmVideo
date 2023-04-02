6be621a731a2e4185ca0f05c389a1666ca8b3e73
NICOLAS ALBUQUERQUE <123500218+NicolasAlbuquerque@users.noreply.github.com>
2023-04-02 07:00:25 -0300

    aula 6 do curso de Orientação a Objetos.

--- Aula06/src/aula06/Aula06.java
+++ Aula06/src/aula06/Aula06.java
@@ -3,7 +3,13 @@ package aula06;
 public class Aula06 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ControleRemoto c1= new ControleRemoto();
+		
+		c1.ligar();
+		c1.maisVolume();
+		c1.ligarMudo();
+		c1.play();
+		c1.abrirMenu();
 
 	}
 
--- Aula06/src/aula06/ControleRemoto.java
+++ Aula06/src/aula06/ControleRemoto.java
@@ -17,103 +17,120 @@ public class ControleRemoto implements Controlador{
 	}
 
 
-	public int getVolume() {
+	private int getVolume() {
 		return volume;
 	}
 
 
-	public void setVolume(int volume) {
+	private void setVolume(int volume) {
 		this.volume = volume;
 	}
 
 
-	public boolean isLigado() {
+	private boolean getLigado() {
 		return ligado;
 	}
 
 
-	public void setLigado(boolean ligado) {
+	private void setLigado(boolean ligado) {
 		this.ligado = ligado;
 	}
 
 
-	public boolean isTocando() {
+	private boolean getTocando() {
 		return tocando;
 	}
 
 
-	public void setTocando(boolean tocando) {
+	private void setTocando(boolean tocando) {
 		this.tocando = tocando;
 	}
 
+	
+	//metodos abstratos
 
 	@Override
 	public void ligar() {
-		this.setLigado(ligado);
+		this.setLigado(true);
 		
 	}
 
 
 	@Override
 	public void desligar() {
-		// TODO Auto-generated method stub
-		
+		this.setLigado(false);
 	}
 
 
 	@Override
 	public void abrirMenu() {
-		// TODO Auto-generated method stub
-		
+		System.out.println("Está ligada?"+this.getLigado());
+		System.out.println("Está tocando?"+ this.getTocando());
+		System.out.println("volume: "+ this.getVolume());
+		for (int i =1; i <= this.getVolume();i+=10) {
+			System.out.print("|");
+		}
+			
 	}
 
 
 	@Override
 	public void fecharMenu() {
-		// TODO Auto-generated method stub
+		System.out.println("Fechando menu");
 		
 	}
 
 
 	@Override
 	public void maisVolume() {
-		// TODO Auto-generated method stub
+		if(this.getLigado()) {
+			this.setVolume(this.getVolume() + 5);
+		}
 		
 	}
 
 
 	@Override
 	public void menosVolume() {
-		// TODO Auto-generated method stub
+		if (this.getLigado()) {
+			this.setVolume(this.getVolume() - 5);
+		}
 		
 	}
 
 
 	@Override
 	public void ligarMudo() {
-		// TODO Auto-generated method stub
+		if (this.getLigado() && this.getVolume() >0) {
+			this.setVolume(0);
+		}
 		
 	}
 
 
 	@Override
 	public void desligarMudo() {
-		// TODO Auto-generated method stub
+		if (this.getLigado()&& this.getVolume()==0) {
+			this.setVolume(50);
+		}
 		
 	}
 
 
 	@Override
 	public void play() {
-		// TODO Auto-generated method stub
+		if (this.getLigado() && !(this.getTocando())) {
+			this.setTocando(true);
+		}
 		
 	}
 
 
 	@Override
 	public void pause() {
-		// TODO Auto-generated method stub
-		
+		if(this.getLigado() && this.getTocando()) {
+			this.setTocando(false);
+		}
 	}
 	
 	
